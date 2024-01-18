0cc1f003703666ba26f37f198596e9c0093bed06
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-18 09:48:54 +0100

    Usando suspend

--- app/src/main/java/com/example/roomexample/MainActivity.kt
+++ app/src/main/java/com/example/roomexample/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.roomexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.example.roomexample.database.AppDatabase
+import com.example.roomexample.database.entities.User
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            databaseAccess()
+        }
+    }
+
+    suspend fun databaseAccess(){
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "DDBB"
+        ).build()
+
+
+        val userDao = db.userDao()
+
+        var user = User(1, "Antonio", "Mendoza")
+        userDao.insertAll(user)
+        val users: List<User> = userDao.getAll()
+
+        val i = 0
     }
 }
--- app/src/main/java/com/example/roomexample/database/dao/UserDao.kt
+++ app/src/main/java/com/example/roomexample/database/dao/UserDao.kt
@@ -9,19 +9,19 @@ import com.example.roomexample.database.entities.User
 @Dao
 interface UserDao {
     @Query("SELECT * FROM user")
-    fun getAll(): List<User>
+    suspend fun getAll(): List<User>
 
     @Query("SELECT * FROM user WHERE id IN (:userIds)")
-    fun loadAllByIds(userIds: IntArray): List<User>
+    suspend fun loadAllByIds(userIds: IntArray): List<User>
 
     @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
             "apellido LIKE :last LIMIT 1")
-    fun findByName(first: String, last: String): User
+    suspend fun findByName(first: String, last: String): User
 
     @Insert
-    fun insertAll(vararg users: User)
+    suspend fun insertAll(vararg users: User)
 
     @Delete
-    fun delete(user: User)
+    suspend fun delete(user: User)
 
 }
